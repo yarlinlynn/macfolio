b46451e7760b27394de97091602ca324e9ed2815
Yarlin <yarlinlynn@gmail.com>
2026-07-17 18:08:51 +0200

    Dynamically created workspace component for both mobile and desktop screen size using js with relevant styles

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 import { Header } from "./components/header.js";
 import { Dock } from "./components/dock.js";
+import { Workspace } from "./components/workspace.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     Header();
+    Workspace()
     Dock();
 });
 
 window.addEventListener('DOMContentLoaded', () => {
     Header();
+    Workspace();
 });
 
 window.addEventListener('resize', () => {
     Header();
+    Workspace();
 });
