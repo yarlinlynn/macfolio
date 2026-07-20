0fdb05f8faa656d5ccf5866a538a609cabfd54a7
Yarlin <yarlinlynn@gmail.com>
2026-07-20 13:58:51 +0200

    Created index.js file & added dock array to store info of dock to use in dock.js later instead of hard coding data

---
+++ js/constants/index.js
@@ -0,0 +1,118 @@
+
+// DOCK APPS
+export const dock = [
+    {
+        id: "phone",
+        name: "Phone",
+        icon: "/assets/apps/phone.png",    
+        canOpen: true,
+    },
+    {
+        id: "finder",
+        name: "Finder",
+        icon: "/assets/apps/finder.png",    
+        canOpen: true,
+    },
+    {
+        id: "launchpad",
+        name: "Launchpad",
+        icon: "/assets/apps/launchpad.png",    
+        canOpen: false,
+    },
+    {
+        id: "safri",
+        name: "Safri",
+        icon: "/assets/apps/safri.png",    
+        canOpen: false,
+    },
+    {
+        id: "messages",
+        name: "Messages",
+        icon: "/assets/apps/messages.png",    
+        canOpen: false,
+    },
+    {
+        id: "maps",
+        name: "Maps",
+        icon: "/assets/apps/maps.png",    
+        canOpen: false,
+    },
+    {
+        id: "photos",
+        name: "Photos",
+        icon: "/assets/apps/photos.png",    
+        canOpen: false,
+    },
+    {
+        id: "player",
+        name: "Player",
+        icon: "/assets/apps/player.png",    
+        canOpen: false,
+    },
+    {
+        id: "gmail",
+        name: "Gmail",
+        icon: "/assets/apps/gmail.png",    
+        canOpen: true,
+    },
+    {
+        id: "facetime",
+        name: "FaceTime",
+        icon: "/assets/apps/facetime.png",    
+        canOpen: false,
+    },
+    {
+        id: "calendar",
+        name: "Calendar",
+        icon: "/assets/apps/calendar-dektop.png",    
+        canOpen: false,
+    },
+    {
+        id: "reminders",
+        name: "Reminders",
+        icon: "/assets/apps/reminders.png",    
+        canOpen: false,
+    },
+    {
+        id: "preview",
+        name: "Preview",
+        icon: "/assets/apps/preview.png",    
+        canOpen: false,
+    },
+    {
+        id: "notes",
+        name: "Notes",
+        icon: "/assets/apps/notes.png",    
+        canOpen: true,
+    },
+    {
+        id: "appstore",
+        name: "App Store",
+        icon: "/assets/apps/appstore.png",    
+        canOpen: false,
+    },
+    {
+        id: "settings",
+        name: "Settings",
+        icon: "/assets/apps/settings.png",    
+        canOpen: false,
+    },
+    {
+        id: "siri",
+        name: "Siri",
+        icon: "/assets/apps/siri.png",    
+        canOpen: false,
+    },
+    {
+        id: "spotify",
+        name: "Spotify",
+        icon: "/assets/apps/spotify.png",    
+        canOpen: true,
+    },
+    {
+        id: "trash",
+        name: "Trash",
+        icon: "/assets/apps/trash-full.png",    
+        canOpen: false,
+    },
+];
