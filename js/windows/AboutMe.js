3eac6a1c002153ab532ab098896589f931fc4779
Yarlin <yarlinlynn@gmail.com>
2026-08-07 13:55:49 +0200

    Fixed AboutMe component using if statements, ensuring that when individual windows are closed it doesn't cause a re-render of content

--- js/windows/AboutMe.js
+++ js/windows/AboutMe.js
@@ -1,4 +1,5 @@
 
+import { windowState } from "../state/WindowState.js";
 import { windowManager } from "../state/WindowManager.js";
 import WindowWrapper from "../state/WindowWrapper.js";
 
@@ -13,13 +14,24 @@ export function AboutMe(parent=document.body) {
     Images(windowElement, "aboutImage1");
     Images(windowElement, "aboutImage2");
 
-    window.renderContent=function(item) {
+    window.renderContent = function(item) {
         const text = item.children.find(file => file.fileType === "txt");
         const images = item.children.filter(file => file.fileType === "img");
 
-        windowManager.open("aboutText",text);
-        windowManager.open("aboutImage1",images[0]);
-        windowManager.open("aboutImage2",images[1]);
+        // open txt window
+        if(text && !windowState.windows.aboutText.isOpen) {
+            windowManager.open("aboutText",text)
+        }
+
+        // open image window
+        if(images[0] && !windowState.windows.aboutImage2.isOpen) {
+            windowManager.open("aboutImage1", images[0]);
+        }
+
+        // open another image window
+        if(images[1] && !windowState.windows.aboutImage2.isOpen) {
+            windowManager.open("aboutImage2", images[1]);
+        }
     }
 
     window.enableDrag();
